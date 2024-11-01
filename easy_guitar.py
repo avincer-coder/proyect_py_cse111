a372427fdd4eeee56687315eda162d9eeef7c331
avincer-coder <gar23078@byui.edu>
2024-10-31 18:02:24 -0600

    Updates 1

--- easy_guitar.py
+++ easy_guitar.py
@@ -43,6 +43,27 @@ tab_star_intermediate = [
     "E|----3-------------|",
 ]
 
+songs_outside = {
+        "Easy Level": ["star easy", "let her go easy", "your beautiful easy"],
+        "Intermediate Level": ["star intermediate", "let her go intermediate", "your beautiful intermediate"],
+        "Difficult Level": ["star hard ", "let her go hard", "your beautiful hard"]
+    }
+
+#Primer main_menu opciones para random o escojer cancion
+#La parte donde seleccionamos la cancion debe ir en una funcion 
+# necesitramos una funcion para escojer una cancion random
+# 4 tests para las funciones respectivamente
+
+def main_menu():
+    print("\n--- Main Menu ---")
+    print("1. Random song and level select!")
+    print("2. Choose level and song")
+    choice = input("Select an option: ")
+    return int(choice)
+
+def random():
+    print("TEXTO RANDOM")
+
 
 
 def show_menu():
@@ -59,55 +80,62 @@ def show_songs(level):
         "Difficult Level": ["star hard ", "let her go hard", "your beautiful hard"]
     }
     
-    print(f"\n--- Songs for {level} ---")
+    # print(f"\n--- Songs for {level} ---")
     easy_songs = songs[level]
-
-# Imprimir las canciones guardadas
     print(f"\n--- Songs for {level} ---")
-    for song in easy_songs:
-        print(song)
-    print(easy_songs)
+    for index, song in enumerate(easy_songs):
+        print(f"{index+1}. {song}")
+    return easy_songs
+
+    
         
     
 
 def main():
-    while True:
-        show_menu()
-        choice = input("Select an option: ")
+    choice_main_menu = main_menu()
+    print(choice_main_menu)
+    if choice_main_menu == 1:
+        random()
+    else:
+        print("Logica escojer")
 
-        if choice == "1":
-            level = "Easy Level"
-            print(f"You selected {level}.")
-            show_songs(level)
+
+    # while True:
+    #     show_menu()
+    #     choice = input("Select an option: ")
+    #     selected_lvl_song = ""
+    #     if choice == "1":
+    #         level = "Easy Level"
+    #         print(f"You selected {level}.")
+    #         selected_lvl_song = show_songs(level)
             
-        elif choice == "2":
-            level = "Intermediate Level"
-            print(f"You selected {level}.")
-            show_songs(level)
-            #no me regresa la cancion
-        elif choice == "3":
-            level = "Difficult Level"
-            print(f"You selected {level}.")
-            show_songs(level)
-        elif choice == "4":
-            print("Exiting the menu...")
-            break
-        else:
-            print("Invalid option, please try again.")
+            
+    #     elif choice == "2":
+    #         level = "Intermediate Level"
+    #         print(f"You selected {level}.")
+    #         selected_lvl_song = show_songs(level)
+    #         #no me regresa la cancion
+    #     elif choice == "3":
+    #         level = "Difficult Level"
+    #         print(f"You selected {level}.")
+    #         selected_lvl_song = show_songs(level)
+    #     elif choice == "4":
+    #         print("Exiting the menu...")
+    #         break
+    #     else:
+    #         print("Invalid option, please try again.")
 
-        # Song selection
-        song_choice = input("Select a song by number: ")
-        if song_choice in ["1", "2", "3"]:
-            print(f"You selected {song_choice}.")
-            if song_choice == "1":
-                for line in tab_star_easy:
-                    print(line)
-            elif song_choice == "2":
-                for line in tab_star_intermediate:
-                    print(line)
-        elif song_choice == "4":
-            continue
-        else:
-            print("Invalid song selection, returning to level menu.")
+    #     # Song selection
+        
+    #     song_choice = int(input("Select a song by number: "))
+    #     one_selected_song = selected_lvl_song[song_choice-1]
+    #     print(one_selected_song)
+    #     if one_selected_song == "star easy":
+    #         for line in tab_star_easy:
+    #             print(line)
+    #     if one_selected_song == "star intermediate":
+    #         for line in tab_star_intermediate:
+    #             print(line)
+    print("esto een main")
 if __name__ == "__main__":
     main()
